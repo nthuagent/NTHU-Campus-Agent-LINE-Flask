a6a2074e47f0c6f17cad931166645d81bb44f176
Wei <kiwi072400.lin@gmail.com>
2022-12-10 21:13:37 +0800

    Update baseAPI.py

--- API/baseAPI.py
+++ API/baseAPI.py
@@ -5,6 +5,7 @@ class API:
     def __init__(self):
         self.URL = os.environ['GINIP'] + '/api/v1'
         #self.URL = 'http://127.0.0.1:5000/api/v1'
+        self.liff_URL = os.environ['GINIP'] + '/api/v1'
         
     def ping(self):
         url = self.URL + '/ping'
